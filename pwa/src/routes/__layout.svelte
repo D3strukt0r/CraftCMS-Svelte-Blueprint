b945d04b53771cd842c94bd29e4f62793e43ba47
D3strukt0r <dev@d3strukt0r.me>
2022-01-13 01:59:35 +0100

    Add web-vitals

--- pwa/src/routes/__layout.svelte
+++ pwa/src/routes/__layout.svelte
@@ -1,5 +1,26 @@
-<script>
+<script context="module">
+    /** @type {import('@sveltejs/kit').Load} */
+    export async function load({ url, params }) {
+        return {
+            props: {
+                params,
+                path: url.pathname,
+            }
+        };
+    }
+</script>
+<script lang="ts">
     import '../app.css';
+
+    import { onMount } from 'svelte';
+    import { webVitals } from '$lib/webvitals';
+
+    export let path: string;
+    export let params: Record<string, string>;
+
+    onMount(() => {
+        webVitals({ path, params });
+    });
 </script>
 
 <slot></slot>
